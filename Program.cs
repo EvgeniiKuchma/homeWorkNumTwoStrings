89ef046e55af55df64bbde6476223cb6ce28f940
Евгений Кучма <desferto87@gmail.com>
2024-06-16 20:19:14 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkNumTwoStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string userName;
+            int userAge;
+            string userZodiacSign;
+            string userProfession;
+
+            Console.Write("Укажите ваше имя: ");
+            userName = Console.ReadLine();
+            Console.Write("Укажите свой возраст: ");
+            userAge = Convert.ToInt32( Console.ReadLine() );
+            Console.Write("Укажите свой знак зодиака: ");
+            userZodiacSign = Console.ReadLine();
+            Console.Write("Укажите вашу профессию: ");
+            userProfession = Console.ReadLine();
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Вас зовут {userName}, вам {userAge} лет, " +
+                $"вы {userZodiacSign}, ваша профессия {userProfession}, " +
+                $"благодарим что прошли опрос (-:");
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("homeWorkNumTwoStrings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("homeWorkNumTwoStrings")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d43013b2-7000-4c50-b056-f0f5da63408b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
